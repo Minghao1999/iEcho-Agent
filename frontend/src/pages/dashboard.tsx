e5fde2a83995f132b4926e69fb48185f3ac4ce17
Faizack <faizack619@gmail.com>
2024-04-04 02:09:23 +0530

    added real time update in message

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,18 +1,20 @@
 import { useEffect, useMemo } from "react";
 import toast from "react-hot-toast";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { io } from "socket.io-client";
 import Chat from "../components/Chat";
 import Header from "../components/menuHeader";
 import MessageMenu from "../components/messageMenu";
-import { updateLastMessage } from "../redux/reducer/contactReducer";
+import { addContact, updateLastMessage } from "../redux/reducer/contactReducer";
 import { addMessage } from "../redux/reducer/messageReducer";
 import { BotMessageResponse } from "../types/api";
 import { MessageSocket } from "../types/message";
+import { RootState } from "../redux/store";
 
 const Dashboard = () => {
   const socket = useMemo(() => io(`${import.meta.env.VITE_SERVER_SOCKET_IP}`), []);
   const dispatch = useDispatch();
+  const AllContact = useSelector((state: RootState) => state.contactReducer.contacts);
 
   useEffect(() => {
     const handleSocketConnect = () => {
@@ -23,7 +25,6 @@ const Dashboard = () => {
       const milliseconds = Number(data.timestamp) * 1000;
       const date = new Date(milliseconds);
       const formattedDate = date.toLocaleString("en-US");
-      console.log("Date", formattedDate);
 
       const msg = {
         _id: data.id,
@@ -31,7 +32,10 @@ const Dashboard = () => {
         text: data.data.text,
         timestamp: formattedDate,
       };
-
+      const contactExists = AllContact.some(contact => contact.phonenumber === data.from);
+      if (!contactExists) {
+        dispatch(addContact({_id: data.id, phonenumber: data.from,name:data.name,lastmessage:"",setting:"auto"}));
+      }
       const notification = `Msg from ${data.from}\nText: ${data.data.text}\nTime: ${formattedDate}`;
       dispatch(updateLastMessage({ phone: data.from, lastmessage: data.data.text }));
       dispatch(addMessage(msg));
@@ -57,7 +61,7 @@ const Dashboard = () => {
       socket.off("message", handleIncomingMessage);
       socket.off("bot-message", handleBotMessage);
     };
-  }, [socket, dispatch]);
+  }, [socket, dispatch, AllContact]);
 
   return (
     <div className="dashboard">
