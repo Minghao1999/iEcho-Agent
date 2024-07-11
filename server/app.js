d3c7d81bf9c8fd58c92e4b0b8f4528d5aee26f69
Minghao1999 <m_sun11@u.pacific.edu>
2024-07-11 18:38:54 -0500

    Auto-commit JSX on save for 227

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const app = express();
 // Environment
 const port = Number(process.env.port) || Number(5003);
 // const host = process.env.host || "127.0.0.1";
-const host = process.env.host || "172.31.19.233";
+const host = process.env.host || "3.143.110.86";
 const MongoDB_URL = process.env.MongoDB_URL;
 
 app.use(express.json());
