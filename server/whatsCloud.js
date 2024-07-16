dc4a78b7d49e7b778cada894f977ce3b55e6d17e
Minghao1999 <m_sun11@u.pacific.edu>
2024-07-15 20:40:48 -0500

    Auto-commit JSX on save for 227

--- server/whatsCloud.js
+++ server/whatsCloud.js
@@ -21,7 +21,7 @@ const app = express();
 const server = createServer(app); // Create HTTP server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://18.216.16.95:5173",
     methods: ["GET", "POST"],
     credentials: true,
   },
